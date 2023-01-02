7ea54219dfccbc8f3385a861e1ab2adc519d6b29
Viktor Z <all.vitya.z@mail.ru>
2023-01-02 14:36:45 +0500

    add Enter A and B

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int A_var, B)var = 0;
+
 int main()
 {
+    cout << "Enter A" << endl;
+    cin >> A_var;
+
+    cout << "Enter B" << endl;
+    cin >> B_var;
+
+    cout << A_var + B_var << endl;
+
+    return 0;
 }
